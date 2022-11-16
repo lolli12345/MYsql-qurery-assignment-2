54f508335ebcce394380e227adb63cfb84d36102
lokesh dhakad <41200239+lolli12345@users.noreply.github.com>
2022-11-16 22:55:39 +0530

    Add files via upload

--- MySQLQuries/MySQLQuries.session.sql
+++ MySQLQuries/MySQLQuries.session.sql
@@ -1,3 +1,16 @@
+CREATE TABLE countries ( 
+COUNTRY_ID varchar(2),
+COUNTRY_NAME varchar(40)
+CHECK(COUNTRY_NAME IN('Italy','India','China')) ,
+REGION_ID decimal(10,0)
+);
+
+
+desc countries;
+
+
+create table country_new as select * from countries;
+
 desc country_new;
 
 
